9f1a5a7a94b50b9d8334444cf5afdded19185dd0
Paulina <cudo.paulina@gmail.com>
2019-06-11 08:43:50 +0200

    fix phi and F1,G1,H1

--- meshfree.py
+++ meshfree.py
@@ -11,12 +11,12 @@ E = 10
 r = 0.1
 sigma = 0.5
 theta = 0.8
-c = 3  # shape parameter (????)
+c = 0.1 # shape parameter (????)
 
 
-phi = np.zeros(N)
-dphi = np.zeros(N)
-ddphi = np.zeros(N)
+phi = np.zeros([N+1, M+1])
+dphi = np.zeros([N+1, M+1])
+ddphi = np.zeros([N+1, M+1])
 
 F1 = np.zeros([N+1, M+1])
 H1 = np.zeros([N+1, M+1])
@@ -36,21 +36,22 @@ i = np.where(time == t)[0][0]
 
 V[:, i] = np.array([i-E if i > 0 else 0 for i in S])  # Call - (max(S-E, 0)), Put - (max(E-S,0))
 #######################################################################
-for j in range(0, N):
-    phi[j] = np.sqrt(c**2+np.abs(S-S[j])**2)
-    dphi[j] = 1/phi[j]*(S-S[j])
-    ddphi[j] = 1/phi[j]**3*(c**2+abs(S-S[j])**2+(S-S[j]))
+for j in range(0, N+1):
+    phi[:, j] = np.sqrt(c**2+np.abs(S-S[j])**2)
+    dphi[:, j] = 1/phi[:, j]*(S-S[j])
+    ddphi[:, j] = 1/phi[:, j]**3*(c**2+abs(S-S[j])**2+(S-S[j]))
 
-lamb[:, i] = 1/phi*V[:, i]
+lamb[:, i] = 1/phi[:, i]*V[:, i]
 
-for j in range(0, N):
-    F1[j] = 1/2 * sigma**2 * S**2*ddphi+ r*S*dphi - r*phi
-    H1[j] = 1+ (1-theta)*dt* F1
-    G1[j] = 1-theta*dt*F1
+for j in range(0, N+1):
+    F1[:, j] = 1/2 * sigma**2 * S**2*ddphi[:,j]+ r*S*dphi[:,j] - r*phi[:,j]
+    H1[:, j] = 1+ (1-theta)*dt* F1[:,j]
+    G1[:, j] = 1-theta*dt*F1[:,j]
 
 while t > 0:
     i = np.where(time == t)[0][0]
-    lamb[:, i-1] = G1 / lamb[:, i]*H1
+    lamb[:, i-1] = G1[:,i] / lamb[:, i]*H1[:,i]
     t = t-dt
 
-V[:, 0] = np.sum(lamb[:, 0]*phi[j])
+V[:, 0] = np.sum(lamb[:, 0]*phi[:,0])
+
