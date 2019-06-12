98c56534718ae2192a7fc6b93c232805e50f5bf8
Paulina <34506586+Czudo@users.noreply.github.com>
2019-06-12 18:43:50 +0200

    działa

--- meshfree.py
+++ meshfree.py
@@ -5,71 +5,43 @@ from mpl_toolkits.mplot3d import Axes3D
 #V - cena opcji
 
 #  initial conditions
-N = 121  # punkty kolokacji
+N = 100  # punkty kolokacji
 S_max = 30
 T = 0.5  # horyzont czasowy
 M = 100  # liczba kroków czasowych
 E = 10
 r = 0.05
 sigma = 0.2
-theta = 0.5
+theta = 0.02
 c = 0.01  # shape parameter (????)
+V = np.zeros([N, M])
 
-
-phi = np.zeros([N, M])
-dphi = np.zeros([N, M])
-ddphi = np.zeros([N, M])
-
-F1 = np.zeros([N, M])
-H1 = np.zeros([N, M])
-G1 = np.zeros([N, M])
+phi = np.zeros([N, N])
+R = np.zeros([N, N])
 
 dS = S_max / N
 dt = T / M
 
-time = np.arange(dt, T + dt, dt)
-S = np.arange(0, S_max, dS)
-
-V = np.zeros([N, M])
-lamb = np.zeros([N, M])
-
+time = np.arange(dt, T+dt, dt)
+S = np.arange(dS, S_max+dt, dS)
 i = np.where(time == T)[0][0]
 
 V[:, i] = np.array([k - E if k - E > 0 else 0 for k in S])  # Call - (max(S-E, 0)), Put - (max(E-S,0))
-print([k - E if k -E > 0 else 0 for k in S])
-#######################################################################
-#?????????????????????????????????????????????????????????????????????
-for j in range(M):
-    phi[:, j] = np.sqrt(c**2 + np.abs(S - S[j])**2)
-    dphi[:, j] = (c**2 + np.abs(S - S[j])**2)**(-1/2) * (S - S[j])
-    ddphi[:, j] = (c**2 + np.abs(S - S[j])**2)**(-3/2) * (c**2+abs(S-S[j])**2 + (S-S[j]))
-#?????????????????????????????????????????????????????????????????????
 
-lamb[:, i] = 1/phi[:, i]*V[:, i]
+for i in range(N):
+    for j in range(N):
+        phi[i,j] = np.exp(-np.abs(S[i]-S[j])**2/c**2)
+        R[i,j] = 1/2*sigma**2*S[i]**2*((4*(S[i]-S[j])**2-2*c**2)/c**4)*phi[i,j] + r*S[i]*(-2*(S[i]-S[j])/c**2)*phi[i,j] -r*phi[i,j]
 
-for j in range(M):
-    F1[:, j] = 1/2*sigma**2*S**2*ddphi[:, j] + r*S*dphi[:, j] - r*phi[:, j]
-    H1[:, j] = 1 + (1-theta)*dt*F1[:, j]
-    G1[:, j] = 1 - theta*dt*F1[:, j]
-
-
-for t in time:
-    if t!=T:
-        i = np.where(time == t)[0][0]
-        print(i)
-        lamb[:, i] = G1[:, i+1] / lamb[:, i+1]*H1[:, i+1]
-
-
-V[:, 0] = np.sum(lamb[:, 0]*phi[:, 0])
-print(V[:, 0] )
+M1 = np.matmul(phi, np.linalg.inv(phi-(1-theta)*dt*R))
+M2 = np.matmul(phi+theta*dt*R, np.linalg.inv(phi))
+M = np.linalg.inv(np.matmul(M1, M2))
+for i in range(1, N):
+    V[:, N-i-1] = np.matmul(V[:, N-i],M)
 
 xs, ys = np.meshgrid(time, S)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 ax.plot_surface(xs, ys, V, alpha=0.7, color='blue')
-plt.show()
-
-plt.figure()
-plt.plot(S,V[:,0])
 plt.show()
